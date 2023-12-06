e066e005d283c4d0517785cfaabdb921606a75b4
tanner-windows <dtlucas7@gmail.com>
2023-12-06 10:11:40 -0600

    stolen solution womp womp

---
+++ day2/part-2-reddit.py
@@ -0,0 +1,31 @@
+import sys
+
+# https://github.com/matheusstutzel/adventOfCode/blob/main/2023/02/p2.py
+# using the reddit solution because I'm not good enough to do it myself :D
+
+def possible(l):
+    maxV = {
+        "r":0,
+        "g": 0,
+        "b": 0
+    }
+    for w in l.split(","):
+        count, color = w.strip().split(" ")
+        count = int(count)
+        color = color[0]
+        maxV[color] = max(maxV[color], count)
+    return maxV
+sum=0
+
+for line in sys.stdin:
+    line = line.strip()
+    game, x = line.split(':')
+    game = int(game[5:])
+    result = [possible(k.strip()) for k in x.split(";")]
+    r=g=b=0
+    for i in result:
+        r = max(r, i['r'])
+        g = max(g, i['g'])
+        b = max(b, i['b'])
+    sum = sum + (r*g*b)
+print(sum)
